28e9215969cbf602b8ed9f5499f03fed128af735
rishaimishra <rishimishra7872@gmail.com>
2025-09-04 17:30:02 +0530

    cors modified

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+     'allowed_origins' => [
+        'http://localhost:5173', // Your local development
+        'https://your-react-domain.com', // Your React app domain
+        'https://vyapar.srvtechservices.com' // Your Laravel domain
+    ],
 
     'allowed_origins_patterns' => [],
 
