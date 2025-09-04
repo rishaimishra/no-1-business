7462ef80928f4a2f10eae68a6ce7b9abacb648d6
rishaimishra <rishimishra7872@gmail.com>
2025-09-04 17:27:02 +0530

    base url modified

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,12 +11,12 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @routes
+        {{-- @routes
         @viteReactRefresh
         @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
-        @inertiaHead
+        @inertiaHead --}}
     </head>
     <body class="font-sans antialiased">
-        @inertia
+        {{-- @inertia --}}
     </body>
 </html>
